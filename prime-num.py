3a60e18d0003fe4c6becc249856675f76c5ba97e
Salah eddine berret <146879159+SBerret3301@users.noreply.github.com>
2023-10-19 01:59:58 +0100

    Add files via upload

---
+++ prime-num.py
@@ -0,0 +1,10 @@
+t = []
+x = int(input("enter a number : "))
+for i in range (1,x) :
+    a = 0
+    for j in range (1,i+1) :
+        if i % j == 0 :
+            a = a + 1
+    if a == 2 :
+        t.append(i)
+print("prime numbers less than",x,"are :",t)
